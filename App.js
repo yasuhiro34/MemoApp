007006dd7839437ef796de01a4ae0e4c51ffb188
yasuhiro <yasuhiro@yasuhiro-no-MacBook.local>
2018-07-19 23:27:15 +0900

    Create Memolist

--- App.js
+++ App.js
@@ -1,12 +1,75 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BodyText from './src/elements/BodyText';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello!</Text>
+
+        <View style={styles.appbar}>
+          <View>
+            <Text style={styles.appbarTitle}>
+            MEMOT
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.memoList}>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>
+            講座のアイテム
+            </Text>
+            <Text style={styles.memoDate}>
+            2018/07/19
+            </Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>
+            講座のアイテム
+            </Text>
+            <Text style={styles.memoDate}>
+            2018/07/19
+            </Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>
+            講座のアイテム
+            </Text>
+            <Text style={styles.memoDate}>
+            2018/07/19
+            </Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>
+            講座のアイテム
+            </Text>
+            <Text style={styles.memoDate}>
+            2018/07/19
+            </Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>
+            講座のアイテム
+            </Text>
+            <Text style={styles.memoDate}>
+            2018/07/19
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.memoAppButton}>
+          <Text style={styles.memoAddButtonTitle}>
+          +
+          </Text>
+        </View>
       </View>
+
     );
   }
 }
@@ -14,8 +77,66 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
+  },
+  memoAppButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  memoAddButtonTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#FFF',
+  },
+  memoList: {
+    width: '100%',
+    flex: 1,
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    backgroundColor: '#FFF',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 14,
+    color: '#a2a2a2',
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
---
+++ src/elements/BodyText.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class BodyText extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>
+          {this.props.children}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#000',
+    backgroundColor: '#eee',
+  },
+});
+
+
+export default BodyText;
