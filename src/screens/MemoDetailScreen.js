453c6d4fe1e25dc08926ad2fa9ff5251f06afb55
yasuhiro <yasuhiro@yasuhiro-no-MacBook.local>
2018-07-22 14:29:40 +0900

    Style MemoDetailScreen

---
+++ src/screens/MemoDetailScreen.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class MemoDetailScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <View style={styles.memoheader}>
+            <View style={styles.memoheaderContent}>
+              <Text style={styles.memoheaderTitle}>
+              講座のアイデア
+              </Text>
+              <Text style={styles.memoheaderDate}>
+              2018/07/21
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.memoContent}>
+          <Text>
+           講座のアイデアです。
+          </Text>
+        </View>
+
+        <CircleButton color="white" style={styles.editButton}>
+        +
+        </CircleButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoheader: {
+    height: 100,
+    backgroundColor: '#17313C',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  memoheaderTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 4,
+  },
+  memoheaderDate: {
+    fontSize: 12,
+    color: '#fff',
+  },
+  memoContent: {
+    paddingTop: 30,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  editButton: {
+    top: 75,
+  },
+});
+
+export default MemoDetailScreen;
