54a8485c4fd7b125f9813c20de661e3d3dd4f5f4
yasuhiro <yasuhiro@yasuhiro-no-MacBook.local>
2018-07-25 23:56:26 +0900

    Hide firebase certificate

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight} from 'react-native';
+import firebase from 'firebase';
 
 class LoginScreen extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  // eslint-disable-next-line
+  handleSubmit() {
+    const config = {
+      apiKey: 'AIzaSyBpRpqTXwAYNKa75uvOlqu1gEaMBikbtE4',
+      authDomain: 'memoapp-209ec.firebaseapp.com',
+      databaseURL: 'https://memoapp-209ec.firebaseio.com',
+      projectId: 'memoapp-209ec',
+      storageBucket: 'memoapp-209ec.appspot.com',
+      messagingSenderId: '875297125426',
+    };
+    firebase.initializeApp(config);
+    // this.props.navigation.navigate('Home')
+
+    // log in!!
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           ログイン
         </Text>
-        <TextInput style={styles.input} value="Email Address" />
-        <TextInput style={styles.input} value="Password" />
-        <TouchableHighlight style={styles.Button} onPress={() => {}} underlayColor="#C70F66">
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={(text) => { this.setState({ email: text}); }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => { this.setState({ password: text}); }}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <TouchableHighlight style={styles.Button} onPress={this.handleSubmit.bind(this)}>
           <Text style={styles.buttonTitle}>
             ログインする
           </Text>
