5fc3b0d4794860e1140464f948a596c713fc78c0
yasuhiro <yasuhiro@yasuhiro-no-MacBook.local>
2018-07-27 22:41:54 +0900

    Get memoList from firestore

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -13,7 +13,7 @@ class LoginScreen extends React.Component {
     firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
       .then((user) => {
         console.log('success!', user);
-        this.props.navigation.navigate('Home', { currentUser: user});
+        this.props.navigation.navigate('Home');
       })
       .catch((error) => {
         console.log('error!', error);
