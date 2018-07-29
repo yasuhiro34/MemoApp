158544d6f11cd1da9c6537fef95d4559ecde18c3
yasuhiro <yasuhiro@yasuhiro-no-MacBook.local>
2018-07-29 17:32:11 +0900

    Save change on firestore

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableHighlight, FlatList } from 'react-native';
 
+const dateString = (date) => {
+  const str = date.toISOString();
+  return str.split('T')[0];
+};
+
 class MemoList extends React.Component {
   renderMemo({ item }) {
     console.log(item);
     return (
-      <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail'); }}>
+      <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail', { memo: item }); }}>
         <View style={styles.memoListItem}>
           <Text style={styles.memoTitle}>
-            {item.body}
+            {item.body.substring(0, 10)}
           </Text>
           <Text style={styles.memoDate}>
-          2018/07/19
+            {dateString(item.createdOn)}
           </Text>
         </View>
       </TouchableHighlight>
